78021523b18acc36440209f7226309ba2538d20f
JuanAlvarezB <60496780+JuanAlvarezB@users.noreply.github.com>
2021-04-14 07:38:39 -0500

    Update SearchCoursePage.java

--- src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
@@ -16,20 +16,7 @@ public class SearchCoursePage extends PageObject {
     public static final Target NAME_COURSE = Target.the("Obtener Palabra traducida")
             .located(By.xpath("//*[@id='page-header']/div/div/div[2]/div[1]/div[1]/a/div/div"));
 
-/*
 
-    public static final Target INPUT_COURSE = Target.the("Buscar el curso")
-            .located(By.id("coursesearchbox"));
-
-    public static final Target BUTTON_GO = Target.the("Dar click en el boton ir")
-            .located(By.xpath("//button[@class='btn btn-secondary']"));
-
-    public static final Target SELECT_COURSE = Target.the("Seleccionar el curso Recursos Automatizacion Bancolombia")
-            .located(By.xpath("//div[@class='course-title']"));
-
-
-
-*/
 
 }
 
